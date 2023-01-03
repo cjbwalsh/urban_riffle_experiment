1b51f08b3b14172487b7811e4e9a30a5168ca69a
cjbwalsh_rps <cwaslh@unnimelb.edu.au>
2023-01-03 17:27:38 +1100

    first draft: analysis complete

---
+++ nbinom_ba023811_cii_2000iter_ll.R
@@ -0,0 +1,103 @@
+setwd("~/git/urban_riffle_experiment")
+
+library(cmdstanr)
+check_cmdstan_toolchain(fix = TRUE, quiet = TRUE)
+rand_seed = 9430572
+
+source("https://tools.thewerg.unimelb.edu.au/documents/mwstr/mwstr_functions.R")
+source("https://tools.thewerg.unimelb.edu.au/data/mwbugs/bugDatabaseFunctions.R")
+
+## Load data: ultimately from OSF
+# library(osfr); library(dplyr)
+# if(!"data" %in% dir()){system("mkdir data")}
+# if(!"wq_data_compiled.xlsx" %in% dir("data")){
+# wq_files <- osf_retrieve_node("4ywvq") %>% osf_ls_files()
+# osf_download(wq_files[wq_files$name == "wq_data_compiled.xlsx",], path = "data")
+# }
+# compile data 
+sites <- readxl::read_excel("~/uomShare/wergStaff/ChrisW/git-data/urban_riffle_experiment/data_for_model.xlsx", sheet = "sites")
+samples <- readxl::read_excel("~/uomShare/wergStaff/ChrisW/git-data/urban_riffle_experiment/data_for_model.xlsx", sheet = "samples")
+biota <- readxl::read_excel("~/uomShare/wergStaff/ChrisW/git-data/urban_riffle_experiment/data_for_model.xlsx", sheet = "biota")
+subcs <- sqlQuery(paste0("SELECT site, reach FROM subcs WHERE reach IN ('",
+                         paste(substr(sites$sitecode,1,nchar(sites$sitecode) -1),
+                               collapse = "', '"), "');"), "mwstr")
+load("~/uomShare/wergStaff/ChrisW/git-data/mwstr/mwstr_v12_corrections/imp_subcs.rda")
+sites$site <- subcs$site[match(substr(sites$sitecode,1,nchar(sites$sitecode) -1),
+                               subcs$reach)]
+sites$ai <- imp_subcs$c_ai[match(sites$site, imp_subcs$site)]
+
+samples$seg <- substr(samples$old_samplecode,nchar(samples$old_samplecode)-1,
+                      nchar(samples$old_samplecode)-1)
+# aggregate(samples$smpcode, by = list(site = samples$sitecode, 
+#                                      t = samples$t, seg = samples$seg), FUN = length)
+samples <- samples[samples$seg == "M",]
+biota <- biota[biota$smpcode %in% samples$smpcode,]
+biota_ct <- as.data.frame(ct(biota$smpcode, biota$shortcode, biota$count))
+biota_ct <- biota_ct[match(samples$smpcode,row.names(biota_ct)),]
+taxa <- readxl::read_excel("~/uomShare/wergStaff/ChrisW/git-data/urban_riffle_experiment/biota_ct.xlsx", sheet = "taxa")
+# # test with subset of taxa 
+# biota_ct <- biota_ct[apply(biota_ct, 2, FUN = function(x) sum(x > 0)) > 100] #9 taxa
+ss_ct <- biota_ct
+for(i in 1:nrow(samples)){
+  ss_ct[i,] <- samples$subsample_perc[i]/100
+}
+for(i in which(biota$coarsepick == 1)){
+  ss_ct[row.names(ss_ct) == biota$smpcode[i], biota$shortcode[i]] <- 1
+}
+samples$ba <- 0
+samples$ba[substr(samples$old_samplecode,1,1) == "3"] <- 2
+samples$ba[substr(samples$old_samplecode,1,1) == "4"] <- 3
+samples$ba[substr(samples$old_samplecode,1,1) == "5"] <- 8
+samples$ba[substr(samples$old_samplecode,1,1) == "6"] <- 11
+# 0 = before, trips 3, 4, 5, 6  = 2,3,8,11, respectively
+samples$ba <- as.vector(scale(samples$ba))
+samples$ci <- as.numeric(sites$exp_treatment[match(samples$sitecode,sites$sitecode)] == "riffle") #0 = control, 1 = riffle
+samples$baci <- samples$ba*samples$ci
+samples$ai <- sites$ai[match(samples$sitecode, sites$sitecode)]
+i_scaled <- scale(log10(samples$ai*100 + 0.1))
+samples$i <- as.vector(i_scaled)
+sites <- sites[order(sites$exp_treatment,sites$ai),]
+sites$site_no <- 1:nrow(sites)
+samples$site_no <- sites$site_no[match(samples$sitecode,sites$sitecode)]
+samples$sample <- substr(samples$old_samplecode,1,nchar(samples$old_samplecode)-1)
+sample_nos <- data.frame(sample = unique(samples$sample))
+sample_nos$sample_no <- 1:nrow(sample_nos)
+samples$sample_no <- sample_nos$sample_no[match(samples$sample, sample_nos$sample)]
+samples$t <- as.numeric(substr(samples$old_samplecode, 1,1))
+samples$bai <- samples$ba * samples$i
+samples$bacii <- samples$baci * samples$i
+samples$spring <- as.integer(substr(samples$old_samplecode,1,1) %in% c(1,3,5))
+u <- model.matrix(~ ba + ci + baci + i + bacii + spring, data = samples)
+sdata <- list(n_obs = nrow(biota_ct),
+              n_taxa = ncol(biota_ct),
+              n_site = nrow(sites),
+              n_sample = nrow(sample_nos),
+              n_pred = ncol(u),
+              n_t = max(samples$t),
+              site_no = samples$site_no,
+              samp_no = samples$sample_no,
+              t = samples$t,
+              u = u,
+              c = as.matrix(biota_ct),
+              s = as.matrix(ss_ct)
+)
+mod <- cmdstan_model("nbinom_me_randomsite_fixedmatrix_baci.stan", pedantic = TRUE) 
+ni <- 2000; nt <- 4; nb <- 1000; nc <- 4
+stanfit_i <- mod$sample(data = sdata,
+                        seed = rand_seed, chains = nc,
+                        parallel_chains = nc, iter_warmup = nb,
+                        iter_sampling = ni - nb, refresh = 200)
+# 4400 iters 2.5 h
+#  save csv files rather than the model object to use less RAM
+stanfit_i$save_output_files(
+  dir = "~/uomShare/wergStaff/ChrisW/git-data/urban_riffle_experiment/model_fits/",
+  basename = "fit_nbinom_ba23811_cii_2000iter_ll", timestamp = FALSE, random = FALSE)
+stanfit_i$sampler_diagnostics()
+saveRDS(stanfit_i, file = "~/uomShare/wergStaff/ChrisW/git-data/urban_riffle_experiment/model_fits/fit_nbinom_ba23811_cii_2000iter_ll.rds")
+stanfit_i$diagnostic_summary()
+
+# The above three steps required < 500 Mb RAM
+summ <- stanfit_i$summary() # This took > 2h and needed ~80 Gb Ram
+min(summ$ess_bulk,na.rm=TRUE) # 126 from 1000 iterations
+min(summ$ess_tail,na.rm=TRUE) # 173
+# all diagnostic statistics fine.
--- nbinom_me_randomsite_fixedmatrix_baci.stan
+++ nbinom_me_randomsite_fixedmatrix_baci.stan
@@ -1,6 +1,6 @@
 
 data {
-  int<lower=0> n_obs;         // Number of observations (sample-units)
+  int<lower=0> n_obs;         // Number of observations (sample-units, s-u)
   int<lower=1> n_taxa;        // Number of taxa
   int<lower=1> n_site;        // Number of sites
   int<lower=1> n_sample;      // Number of samples
@@ -15,27 +15,29 @@ data {
 }
 parameters {
   vector[n_pred] mu_gamma;
-  matrix[n_pred,n_taxa] gamma;           // coefficients of predictors in u
-  matrix[n_site,n_taxa] a_si;            // coefficient of site
-  matrix[n_sample,n_taxa] a_sa;            // coefficient of sample
-  matrix[n_t,n_taxa] a_t;             // coefficient of sampling occasion
-  matrix<upper=5>[n_obs,n_taxa] epi_raw; // Constraint for stability
+  matrix[n_pred,n_taxa] gamma;           // beta parameters of fixed effects in u
+  matrix[n_site,n_taxa] a_si;            // coefficient of random site effect
+  matrix[n_sample,n_taxa] a_sa;          // coefficient of random sample effect
+  matrix[n_t,n_taxa] a_t;     // coefficient of random sampling occasion effect
+  matrix<upper=5>[n_obs,n_taxa] epi_raw; // raw s-u error term (upper constraint for stability)
   vector<lower=0>[n_taxa] phi;           // dispersion parameter for each taxon
-  vector<lower=0>[n_taxa] sd_lam;
-  corr_matrix[n_pred] Omega;
-  vector<lower=0>[n_pred] tau;           // prior scale
+  vector<lower=0>[n_taxa] sd_lam;        // sd of random s-u error
+  corr_matrix[n_pred] Omega;             // Hyperprior correlation matrix among taxa
+  vector<lower=0>[n_pred] tau;           // Scale for correlation matrix
 }
 transformed parameters {
   matrix[n_obs,n_taxa] log_lambda;     // Log total count
   matrix[n_obs,n_taxa] epi;            // Abundance noise
    for(i in 1:n_obs){
     for(j in 1:n_taxa){
-      epi[i,j] =  sd_lam[j] * epi_raw[i,j];
+      epi[i,j] =  sd_lam[j] * epi_raw[i,j];  //stable reparameterization for epi
   }
   }
   for(i in 1:n_obs){
      for(j in 1:n_taxa){
-      log_lambda[i,j] = a_si[site_no[i],j] +  a_sa[samp_no[i],j] +  a_si[t[i],j] +  u[i,] * gamma[,j] + epi[i,j]; 
+       //The linear model
+      log_lambda[i,j] = a_si[site_no[i],j] +  a_sa[samp_no[i],j] +  
+                        a_si[t[i],j] +  u[i,] * gamma[,j] + epi[i,j]; 
       }
       }
 }
@@ -51,24 +53,33 @@ model {
    tau ~ exponential(1);
    Omega ~ lkj_corr( 2 );  
    
+   // estimation of correlated beta parameters (assembled in matrix gamma)
    for(i in 1:n_taxa){
    target += multi_normal_prec_lpdf(gamma[,i] | mu_gamma , quad_form_diag(Omega , tau) );
      }
   // Likelihood
   for (i in 1 : n_obs) {
     for(j in 1:n_taxa){
+  // Note this parameterization adds the marginal log-binomial-probability 
+  // resulting from subsampling error to the marginal negative-binomial 
+  // probability of the linear model.  It is equivalent to a slower (x ~50!)
+  // parameterization modelling the marginal binomial and negative-binomial
+  // probabilities separately, by looping through all feasible total counts
+  // given a each count and subsample proportion.
     target += neg_binomial_2_log_lpmf(c[i,j] | log_lambda[i,j] + log(s[i,j]), phi[j]);
    }
   }
 }
 
-// generated quantities {
-//   array[n_obs,n_taxa] real log_lik;  // Estimate of true total abandance in each sample
-//  for (i in 1 : n_obs) {
-//    for(j in 1 : n_taxa){
-//    log_lik[i,j] = neg_binomial_2_log_lpmf(c[i,j] | log_lambda[i,j] + log(s[i,j]), phi[j]);
-//  }
-//  }
-//  }
-// 
+generated quantities {
+  // log-likelihood only used for model comparisons during model development.
+  // The most complex model considered has been used, so model comparisons not reported
+  array[n_obs,n_taxa] real log_lik;  
+ for (i in 1 : n_obs) {
+   for(j in 1 : n_taxa){
+   log_lik[i,j] = neg_binomial_2_log_lpmf(c[i,j] | log_lambda[i,j] + log(s[i,j]), phi[j]);
+ }
+ }
+ }
+
 
